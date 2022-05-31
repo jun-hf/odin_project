3436c2922a16cfc8e68dba6742fe602f2db4a421
jun-hf <fong.junh@gmail.com>
2022-05-31 01:37:58 +0100

    sorting updated

--- Sorting.rb
+++ Sorting.rb
@@ -1,9 +1,26 @@
 class Sorting
+    attr_accessor :arr
     def initialize(arr)
         @arr = arr
     end
 
-    def bubble_sort()
-        
+    def bubble_sort
+        return arr if arr.length == 1
+
+        time = arr.length 
+        time -= 1
+
+        loop do 
+            swapped = false
+
+            for i in 0...time
+                if arr[i] > arr[i+1]
+                    arr[i], arr[i+1] = arr[i+1], arr[i]
+                    swapped = true
+                end
+            end
+            break unless swapped
+        end
+        arr
     end
 end
